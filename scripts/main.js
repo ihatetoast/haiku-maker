6b393b9f87a21f2a0feedb311d516c6a18f596e1
Katy <ihatetoast@gmail.com>
2016-03-25 15:11:10 -0500

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+
+//make three arrays of 5. 7, 5 syllables.
+//build them big, vary serious with humour.
+firstLine =['Little kitten cries,', 'Spring frogs chirp with love,', 'Sad slug slides alone,'];
+secondLine =[];
+thirdLine =[];
+
+//user clicks haiku button.
+//build fcn to randomly choose string from each.
+//return a haiku to user. that's all.
+
+//tools: 
+//array
+//Math.random to come up with index number (array.length)
+//fcn k.i.s.s.
