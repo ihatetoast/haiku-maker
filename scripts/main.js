26a54e9d6e0979b8b97947b541938ac366d4ecae
Katy <ihatetoast@gmail.com>
2016-03-26 11:47:34 -0500

    added more lines

--- scripts/main.js
+++ scripts/main.js
@@ -8,7 +8,8 @@ var haiku =document.querySelector('.haikuMaker');
 haiku.addEventListener('click',function() {
 var firstLine =['Little kitten cries,', 'Spring frogs chirp with love,', 'Sad slug slides alone,',
  'Leaves fall and squirrels dig,','Old dog bays at moon,', 'Monkeys howl and growl,', 'Birds peck dead man\'s eyes,',
- 'Rain falls, the creeks rise,', 'Piglets dance for fun,'];
+ 'Rain falls, the creeks rise,', 'Piglets dance for fun,','Raindrops on roses,', 'Whiskers on kittens,', 'Bright copper kettles',
+ 'Warm woollen mittens'];
 
 var firstRandom =function(){
 	return Math.floor(Math.random()*firstLine.length);
@@ -18,7 +19,9 @@ var line1 = firstLine[firstRandom()];
 
 var secondLine =['on a summer\'s midnight eve,', 'packs of dogs eat pizza bones,', 'lilypads float on the pond,',
 'sandwiches feed lines of ants,', 'berries fall and rot away,', 'soft winds carry spiderlings,', 'wolf corpse feeds blowfly larvae,',
-'dandelions spreading seed,', 'ladybugs show off their spots,'];
+'dandelions spreading seed,', 'ladybugs show off their spots,', 'winter makes abandoned death', 'storms washout bunny babies', 
+'otters play and beavers work,', 'fields of corn drown in weavils', 'crabs crawl out of dead man\'s eyes', 'pandas lose their shit in snow,', 
+'bobcat kittens play with mouse,'];
 
 var secondRandom =function(){
 	return Math.floor(Math.random()*secondLine.length);
@@ -29,7 +32,8 @@ var line2 = secondLine[secondRandom()];
 
 var thirdLine =['making small boys cry.', 'sunshine spills on day.', 'gathering in death.', 'foul-mouthed evergreens.', 
 'bluing skies for you.', 'lovely ladies lie.', 'acorns tumble down.', ' flower petals sigh.', 'weasels eat their young.', 
-'dead fish bob and float.', 'puppies chew big sticks.'];
+'dead fish bob and float.', 'puppies chew big sticks.', 'new year comes too soon.', 'lone wolf howls to mate.',
+'hunter shoots own foot.', 'goldfish feast on toes.', 'children laugh for you.', 'dead rat floats downstream.', 'caterpillar cries.'];
 
 var thirdRandom =function(){
 	return Math.floor(Math.random()*thirdLine.length);
