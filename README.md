678c55d585488e5da485803e03a1da6325cdcf3a
Katy <ihatetoast@gmail.com>
2017-05-16 19:48:54 -0500

    cleaned up code. made it ESsexy

---
+++ README.md
@@ -0,0 +1,6 @@
+#Haiku Maker
+### We make haikus so that you don't have to.
+
+This was my first solo bit of code that was not for  homework. At the time, I was frustrated in a bootcamp and trying to keep up. I took a break from homework to make this. I saw that I did understand, but that I was letting the assignment freeze my brain. Later, when I'd get frustrated again, I thought back to this. 
+
+I like to return and add more phrases. 
--- scripts/main.js
+++ scripts/main.js
@@ -1,74 +1,23 @@
-
-//make three arrays of 5. 7, 5 syllables.
-//build them big, vary serious with humour. mushy to innane
-
-
-
 var haiku =document.querySelector('.haikuMaker');
-haiku.addEventListener('click',function() {
-var firstLine =['Little kitten cries,', 'Spring frogs chirp with love,', 'Sad slug slides alone,',
- 'Leaves fall and squirrels dig,','Old dog bays at moon,', 'Monkeys howl and growl,', 'Birds peck dead man\'s eyes,',
- 'Rain falls, the creeks rise,', 'Piglets dance for fun,','Raindrops on roses,', 'Whiskers on kittens,', 'Bright copper kettles',
- 'Warm woollen mittens'];
+var resetKu = document.querySelector('.resetHaiku');
 
-var firstRandom =function(){
-	return Math.floor(Math.random()*firstLine.length);
-};
-// console.log(firstLine[firstRandom()]);
-var line1 = firstLine[firstRandom()];
+var firstLine =['Little kitten cries,', 'Spring frogs chirp with love,', 'Sad slug slides alone,', 'Leaves fall and squirrels dig,','Old dog bays at moon,', 'Monkeys howl and growl,', 'Birds peck dead man\'s eyes,', 'Rain falls, the creeks rise,', 'Piglets dance for fun,','Raindrops on roses,', 'Whiskers on kittens,', 'Bright copper kettles', 'Warm woollen mittens', 'Spiders eat the fly,', 'Old man swims alone,', 'Grubs eat roots of grass'];
+var secondLine =['on a summer\'s midnight eve,', 'packs of dogs eat pizza bones,', 'lilypads float on the pond,', 'sandwiches feed lines of ants,', 'berries fall and rot away,', 'soft winds carry spiderlings,', 'wolf corpse feeds blowfly larvae,', 'dandelions spreading seed,', 'ladybugs show off their spots,', 'winter makes abandoned death', 'storms washout bunny babies', 'otters play and beavers work,', 'fields of corn drown in weavils', 'crabs crawl out of dead man\'s eyes', 'pandas lose their shit in snow,', 'bobcat kittens play with mouse,', 'when moonlight greets the stray dog', 'hollow trees hide owls and death,', 'seas full of manta ray poop'];
+var thirdLine =['making small boys cry.', 'sunshine spills on day.', 'gathering in death.', 'foul-mouthed evergreens.',  'bluing skies for you.', 'lovely ladies lie.', 'acorns tumble down.', ' flower petals sigh.', 'weasels eat their young.', 'dead fish bob and float.', 'puppies chew big sticks.', 'new year comes too soon.', 'lone wolf howls to mate.','hunter shoots own foot.', 'goldfish feast on toes.', 'children laugh for you.', 'dead rat floats downstream.', 'caterpillar cries.', 'rocks in puddles cry.', 'mothers cry for wine.'];
 
-var secondLine =['on a summer\'s midnight eve,', 'packs of dogs eat pizza bones,', 'lilypads float on the pond,',
-'sandwiches feed lines of ants,', 'berries fall and rot away,', 'soft winds carry spiderlings,', 'wolf corpse feeds blowfly larvae,',
-'dandelions spreading seed,', 'ladybugs show off their spots,', 'winter makes abandoned death', 'storms washout bunny babies', 
-'otters play and beavers work,', 'fields of corn drown in weavils', 'crabs crawl out of dead man\'s eyes', 'pandas lose their shit in snow,', 
-'bobcat kittens play with mouse,'];
+haiku.addEventListener('click',() => {
+	var firstRandom = Math.floor(Math.random()*firstLine.length);
+	var secondRandom = Math.floor(Math.random()*secondLine.length);
+	var thirdRandom = Math.floor(Math.random()*thirdLine.length);
 
-var secondRandom =function(){
-	return Math.floor(Math.random()*secondLine.length);
-};
-// console.log(secondLine[secondRandom()]);
-var line2 = secondLine[secondRandom()];
+	var line1 = firstLine[firstRandom];
+	var line2 = secondLine[secondRandom];
+	var line3 = (thirdLine[thirdRandom]);
 
-
-var thirdLine =['making small boys cry.', 'sunshine spills on day.', 'gathering in death.', 'foul-mouthed evergreens.', 
-'bluing skies for you.', 'lovely ladies lie.', 'acorns tumble down.', ' flower petals sigh.', 'weasels eat their young.', 
-'dead fish bob and float.', 'puppies chew big sticks.', 'new year comes too soon.', 'lone wolf howls to mate.',
-'hunter shoots own foot.', 'goldfish feast on toes.', 'children laugh for you.', 'dead rat floats downstream.', 'caterpillar cries.'];
-
-var thirdRandom =function(){
-	return Math.floor(Math.random()*thirdLine.length);
-};
-// console.log(thirdLine[thirdRandom()]);
-var line3 = (thirdLine[thirdRandom()]);
-
-
-	
-	// console.log("this much works.");
 	document.querySelector('.haiku').innerHTML = ('<div>'+line1+'</div><div>'+line2+'</div><div>'+line3+'</div>');
 });
 
-var resetKu = document.querySelector('.resetHaiku');
-resetKu.addEventListener('click', function(){
+resetKu.addEventListener('click', ()=>{
 	document.querySelector('.haiku').innerHTML = '';
 });
-
-//user clicks haiku button.
-
-//return a haiku to user. that's all.
-
-//tools: 
-//array
-//Math.random to come up with index number (array.length)
-//fcn k.i.s.s.
-
-
-
-
-
-
-
-
-
-
-
 
